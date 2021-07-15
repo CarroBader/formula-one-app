1a09bed1588fea7f808479ecdb51f837e14df9f9
marcus <marcusbader@hotmail.com>
2021-07-15 03:44:21 +0200

    add production server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const path = require('path');
 const app = express();
-
+const https = require('https')
+const request = require('request');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+const host = process.env.HOST || "localhost";
 
 var server = app.listen(
     port,
     console.log(
-        `Server is running on port ${port} `
+        `Server is running on host ${host} with port ${port} `
     )
 );
+const baseUrl = "https://ergast.com/api/f1/";
+const apiPre = "/api/f1/";
+app.get(apiPre + ":path", (req, res) => {
+let url = `${baseUrl}${req.params.path}`
+	request(url, { json: true }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  return res.json(body)
+});
+});
 
-
+app.get(apiPre + ":path/:end", (req, res) => {
+let url = `${baseUrl}${req.params.path}/${req.params.end}`
+	request(url, { json: true }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  return res.json(body)
+});
+});
+app.get(apiPre + ":path/:middle/:end", (req, res) => {
+let url = `${baseUrl}${req.params.path}/${req.params.middle}/${req.params.end}`
+	request(url, { json: true }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  return res.json(body)
+});
+});
 app.use(express.static(path.join(__dirname, '../dist')));
 console.log(__dirname)
 app.get('/*', (req, res) => {
--- src/vars.js
+++ src/vars.js
@@ -1,4 +1,8 @@
-const baseUrl = "http://ergast.com/api/f1/";
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || "localhost";
+const baseUrl = process.env.NODE_ENV == "production" ? `${host}:${port}` :  "http://ergast.com/api/f1/";
+console.log(process.env.NODE_ENV)
 const baseUrlWeather = "https://weatherapi-com.p.rapidapi.com";
 const host = "weatherapi-com.p.rapidapi.com";
 const apiKey = "876d91601bmsh299991229f50559p1bfe00jsn9cfb26a0fe9a";
