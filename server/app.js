9edfc23fbad4c0e23a9f4b0f032ab14e2539e00b
marcus <marcusbader@hotmail.com>
2021-07-15 04:11:25 +0200

    add cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 const https = require('https')
 const request = require('request');
+const cors = require("cors");
+var corsOptions = {
+	origin: 'https://formula-one.herokuapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || "localhost";
