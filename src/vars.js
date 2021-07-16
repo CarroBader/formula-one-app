b135218cd695c6e2256e822ecea60425fd0c64e5
marcus <marcusbader@hotmail.com>
2021-07-16 20:06:39 +0200

    change to production

--- src/vars.js
+++ src/vars.js
@@ -3,7 +3,7 @@ dotenv.config({ path: "../.env" });
 const port = process.env.PORT || 5000;
 const hostname = process.env.HOST || "localhost";
 let baseUrl = "https://forumula-one.herokuapp.com/api/f1/";
-baseUrl = "http://ergast.com/api/f1/";
+//baseUrl = "http://ergast.com/api/f1/";
 const baseUrlWeather = "https://weatherapi-com.p.rapidapi.com";
 const host = "weatherapi-com.p.rapidapi.com";
 const apiKey = "876d91601bmsh299991229f50559p1bfe00jsn9cfb26a0fe9a";
