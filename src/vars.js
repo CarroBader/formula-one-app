5000b11a7e27edf1adc82b1c2d27a4eb18664bd8
marcus <marcusbader@hotmail.com>
2021-07-27 08:14:48 +0200

    fix comment

--- src/vars.js
+++ src/vars.js
@@ -1,5 +1,5 @@
 let baseUrl = "https://formula-one-light.herokuapp.com/api/f1/";
-//baseUrl = "http://ergast.com/api/f1/";
+// baseUrl = "http://ergast.com/api/f1/";
 const baseUrlWeather = "https://weatherapi-com.p.rapidapi.com";
 const host = "weatherapi-com.p.rapidapi.com";
 const apiKey = "876d91601bmsh299991229f50559p1bfe00jsn9cfb26a0fe9a";
