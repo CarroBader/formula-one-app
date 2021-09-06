ce7a3780d6c975d0e089c039416e0c48b1bfabbe
Anton Ingvarsson <ingvarsson.anton@gmail.com>
2021-09-06 18:16:19 +0000

    Resolve "Make last race view responsive"

--- src/components/UpcomingRace/UpcomingRaceNav.vue
+++ src/components/UpcomingRace/UpcomingRaceNav.vue
@@ -3,7 +3,7 @@
     <b-navbar toggleable="md navbar-sub">
         <b-navbar-nav class="navbar-nav-sub">
             <router-link
-              v-for="route in routes"
+              v-for="route in routesToUse"
               :key="route.path"
               :to="route.path"
               class="nav-link nav-link-sub"
@@ -18,10 +18,15 @@
 <script>
 export default {
   name: `UpcomingRaceNav`,
+  props: { isSprintRace: { default: false, type: Boolean } },
   data() {
     return {
-      routes: this.$router.options.routes[0].children,
+      routesToUse: [],
     }
+  },
+  beforeMount() {
+    this.routesToUse = this.$router.options.routes[0].children
+    if (!this.isSprintRace) { this.routesToUse = this.routesToUse.filter((race) => !race.onlyForSprintRaces) }
   },
 }
 </script>
