3146f82b571d26d78dc36b4dc8427ace478633c0
carrobader <carolinebader@hotmail.com>
2022-01-24 09:18:24 +0100

    Fix circuit info design

--- src/components/UpcomingRace/NextRace/NextRaceGrandPrixInfo.vue
+++ src/components/UpcomingRace/NextRace/NextRaceGrandPrixInfo.vue
@@ -1,57 +1,76 @@
 <template>
 	<div>
 		<b-card>
-			<h5 class="next-race-circuit-info-name">
+			<h5 class="next-race-grand-prix-info-name">
 				{{ nextGrandPrix.grand_prix_name }}
 			</h5>
-			<h5 class="next-race-circuit-info-location">Info</h5>
+			<h5 class="next-race-grand-prix-info-location">Info</h5>
 			<table>
 				<tr>
-					<th class="next-race-circuit-info-th">First held:</th>
-					<td class="next-race-circuit-info-td">
+					<th class="next-race-grand-prix-info-th">First held:</th>
+					<td class="next-race-grand-prix-info-td">
 						{{ nextGrandPrix.first_held }}
 					</td>
 				</tr>
 				<tr>
-					<th class="next-race-circuit-info-th">Number of times held:</th>
-					<td class="next-race-circuit-info-td">
+					<th class="next-race-grand-prix-info-th">Number of times held:</th>
+					<td class="next-race-grand-prix-info-td">
 						{{ nextGrandPrix.times_held }}
 					</td>
 				</tr>
 				<tr>
-					<th class="next-race-circuit-info-th">Length:</th>
-					<td class="next-race-circuit-info-td">
+					<th class="next-race-grand-prix-info-th">Length:</th>
+					<td class="next-race-grand-prix-info-td">
 						{{ nextGrandPrix.length }} km
 					</td>
 				</tr>
 				<tr>
-					<th class="next-race-circuit-info-th">Laps:</th>
-					<td class="next-race-circuit-info-td">{{ nextGrandPrix.laps }}</td>
+					<th class="next-race-grand-prix-info-th">Laps:</th>
+					<td class="next-race-grand-prix-info-td">{{ nextGrandPrix.laps }}</td>
 				</tr>
 				<tr>
-					<th class="next-race-circuit-info-th">Most wins teams:</th>
-					<td class="next-race-circuit-info-th"></td>
-				</tr>
-				<tr v-for="tWins in nextGrandPrix.most_wins_team" :key="tWins.team_id">
-					<th class="next-race-circuit-info-th"></th>
-					<td class="next-race-circuit-info-td">
-						{{ tWins.wins }} - &ensp;
-						<span :class="getConstructorColor(tWins.team_id)">{{
-							tWins.team_name
-						}}</span>
+					<th class="next-race-grand-prix-info-th">Most wins teams:</th>
+					<td class="align-end">
+						<span
+							class="next-race-grand-prix-info-th"
+							v-for="(tWins, index) in nextGrandPrix.most_wins_team"
+							:key="tWins.team_id"
+						>
+							<span :class="getConstructorColor(tWins.team_id)">{{
+								tWins.team_name
+							}}</span
+							>&ensp;
+							<span class="next-race-grand-prix-info-span"
+								>( {{ tWins.wins }} )</span
+							>
+							<span
+								v-if="index === 0 && nextGrandPrix.most_wins_team.length > 1"
+							>
+								&ensp;|</span
+							>
+						</span>
 					</td>
 				</tr>
 				<tr>
-					<th class="next-race-circuit-info-th">Most wins drivers:</th>
-					<td class="next-race-circuit-info-th"></td>
-				</tr>
-				<tr
-					v-for="(dWins, index) in nextGrandPrix.most_wins_driver"
-					:key="index"
-				>
-					<th class="next-race-circuit-info-th"></th>
-					<td class="next-race-circuit-info-td">
-						{{ dWins.wins }} - &ensp; {{ dWins.driver_name }}
+					<th class="next-race-grand-prix-info-th">Most wins drivers:</th>
+					<td class="align-end">
+						<span
+							class="next-race-grand-prix-info-th"
+							v-for="(dWins, index) in nextGrandPrix.most_wins_driver"
+							:key="index"
+						>
+							{{ dWins.driver_name }}
+
+							&ensp;
+							<span class="next-race-grand-prix-info-span"
+								>( {{ dWins.wins }} )</span
+							>
+							<span
+								v-if="index === 0 && nextGrandPrix.most_wins_driver.length > 1"
+							>
+								&ensp;|</span
+							>
+						</span>
 					</td>
 				</tr>
 			</table>
@@ -70,6 +89,7 @@ export default {
 	data() {
 		return {}
 	},
+
 	mixins: [getConstructorColorMixin],
 }
 </script>
