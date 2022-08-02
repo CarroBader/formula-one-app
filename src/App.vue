e5e93340dc93a6893eaaaea6fba3181419ef82d6
carrobader <carolinebader@hotmail.com>
2022-08-02 06:58:34 +0200

    Change races api source

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,103 @@
 <template>
-	<div class="default-background-center" v-if="allDataLoaded">
-		<TopNav />
-		<SwiperNav :key="renderKey" />
-		<router-view />
-		<Footer />
-		<BotNav />
-	</div>
+  <div class="default-background-center" v-if="allDataLoaded">
+    <TopNav />
+    <SwiperNav :key="renderKey" />
+    <router-view />
+    <Footer />
+    <BotNav />
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions, mapState } from "vuex"
-import store from "./store/ store"
-import SwiperNav from "./layout/SwiperNav.vue"
+import { mapGetters, mapActions, mapState } from 'vuex'
+import store from './store/ store'
+import SwiperNav from './layout/SwiperNav.vue'
 
-import TopNav from "./layout/TopNav.vue"
-import Footer from "./layout/Footer.vue"
-import BotNav from "./layout/BotNav.vue"
+import TopNav from './layout/TopNav.vue'
+import Footer from './layout/Footer.vue'
+import BotNav from './layout/BotNav.vue'
 
 export default {
-	name: "App",
-	components: {
-		TopNav,
-		Footer,
-		BotNav,
-		SwiperNav,
-	},
-	data() {
-		return {
-			allRaces: null,
-			nextRaceName: null,
-			renderKey: 0,
-		}
-	},
-	computed: {
-		...mapGetters([
-			"allConfirmedRaces",
-			"allDrivers",
-			"allTeams",
-			"upcomingRace",
-			"nextSession",
-		]),
-		...mapState(["raceDataLoaded", "currentRound", "allDataLoaded", "raceNav"]),
-	},
-	watch: {
-		raceDataLoaded(val) {
-			if (val) {
-				this.allRaces = this.allConfirmedRaces
+  name: 'App',
+  components: {
+    TopNav,
+    Footer,
+    BotNav,
+    SwiperNav,
+  },
+  data() {
+    return {
+      allRaces: null,
+      nextRaceName: null,
+      renderKey: 0,
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'allConfirmedRaces',
+      // "allDrivers",
+      // "allTeams",
+      'upcomingRace',
+      'nextSession',
+    ]),
+    ...mapState(['raceDataLoaded', 'currentRound', 'allDataLoaded', 'raceNav']),
+  },
+  watch: {
+    raceDataLoaded(val) {
+      if (val) {
+        this.allRaces = this.allConfirmedRaces
 
-				if (this.upcomingRace && this.upcomingRace.length > 0) return
-				store.dispatch("getUpcomingRace", this.allRaces)
+        if (this.upcomingRace && this.upcomingRace.length > 0) return
+        store.dispatch('getUpcomingRace', this.allRaces)
 
-				if (this.currentRound && this.currentRound !== 0) return
-				store.dispatch("getNextRaceRound", this.upcomingRace)
+        if (this.currentRound && this.currentRound !== 0) return
+        store.dispatch('getNextRaceRound', this.upcomingRace)
 
-				if (this.nextSession && this.nextSession.length > 0) return
-				store.dispatch("getNextSession", this.upcomingRace)
-			}
-		},
-		$route(to, from) {
-			if (from.name === "Race Map") {
-				this.forceRerender()
-			}
-		},
-	},
-	created() {
-		console.log("App - created")
-		console.log("Time", new Date())
-		console.log("App - allConfirmedRaces", this.allConfirmedRaces)
-		console.log("App - allDrivers", this.allDrivers)
-		console.log("App - allTeams", this.allTeams)
-		console.log("App - raceDataLoaded", this.raceDataLoaded)
+        if (this.nextSession && this.nextSession.length > 0) return
+        store.dispatch('getNextSession', this.upcomingRace)
+      }
+    },
+    $route(to, from) {
+      if (from.name === 'Race Map') {
+        this.forceRerender()
+      }
+    },
+  },
+  created() {
+    console.log('App - created')
+    console.log('Time', new Date())
+    console.log('App - allConfirmedRaces', this.allConfirmedRaces)
+    console.log('App - allDrivers', this.allDrivers)
+    console.log('App - allTeams', this.allTeams)
+    console.log('App - raceDataLoaded', this.raceDataLoaded)
 
-		console.log("App - upcomingRace", this.upcomingRace)
-		console.log("App - currentRound", this.currentRound)
-		console.log("App - nextSession", this.nextSession)
-		console.log("App - allDataLoaded", this.allDataLoaded)
-		console.log(
-			"App ------------------------------------------ raceNav",
-			this.raceNav
-		)
+    console.log('App - upcomingRace', this.upcomingRace)
+    console.log('App - currentRound', this.currentRound)
+    console.log('App - nextSession', this.nextSession)
+    console.log('App - allDataLoaded', this.allDataLoaded)
+    console.log(
+      'App ------------------------------------------ raceNav',
+      this.raceNav
+    )
 
-		if (this.allConfirmedRaces && this.allConfirmedRaces.length > 0) return
-		console.log("App - No data getAllRaces yet")
-		this.getAllRaces()
+    if (this.allConfirmedRaces && this.allConfirmedRaces.length > 0) return
+    console.log('App - No data getAllRaces yet')
+    this.getAllRaces()
 
-		if (this.allDrivers && this.allDrivers.length > 0) return
-		console.log("App - No data getAllDrivers yet")
-		this.getAllDrivers()
+    //   if (this.allDrivers && this.allDrivers.length > 0) return
+    //   console.log('App - No data getAllDrivers yet')
+    //   this.getAllDrivers()
 
-		if (this.allTeams && this.allTeams.length > 0) return
-		console.log("App - No data getAllTeams yet")
-		this.getAllTeams()
-	},
-	methods: {
-		...mapActions(["getAllRaces", "getAllDrivers", "getAllTeams"]),
-		forceRerender() {
-			this.renderKey += 1
-		},
-	},
+    //   if (this.allTeams && this.allTeams.length > 0) return
+    //   console.log('App - No data getAllTeams yet')
+    //   this.getAllTeams()
+  },
+  methods: {
+    ...mapActions(['getAllRaces']), //  'getAllDrivers', 'getAllTeams'
+    forceRerender() {
+      this.renderKey += 1
+    },
+  },
 }
 </script>
 
--- src/store/ store.js
+++ src/store/ store.js
@@ -1,232 +1,224 @@
-import Vue from "vue"
-import Vuex from "vuex"
-import axios from "axios"
-import createPersistedState from "vuex-persistedstate"
-import { baseUrl, baseURLF1, hostF1, apiKeyF1 } from "../variables/apiVars"
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import createPersistedState from 'vuex-persistedstate'
+import { baseUrl } from '../variables/apiVars'
 import {
-	addTrackId,
-	addGrandPrixId,
-	changeCountryName,
-	notWantedSessions,
-	newSessionType,
-} from "../variables/replaceVars"
+  addTrackId,
+  addGrandPrixId,
+  changeCountryName,
+  notWantedSessions,
+  newSessionType,
+} from '../variables/replaceVars'
 
 Vue.use(Vuex)
 
+const todaysDate = new Date()
+
 // If you want to clear createPersistedState sessionStorage.clear()
 export default new Vuex.Store({
-	plugins: [
-		createPersistedState({
-			storage: window.sessionStorage,
-		}),
-	],
-	state: {
-		allDataLoaded: false,
-		raceDataLoaded: false,
-		raceNav: "Last Race",
-		standingsNav: "Driver Standings",
-		allConfirmedRaces: [],
-		allDrivers: [],
-		allTeams: [],
-		upcomingRace: {},
-		currentRound: 0,
-		nextSession: {},
-	},
-	getters: {
-		allConfirmedRaces: (state) => state.allConfirmedRaces,
-		allDrivers: (state) => state.allDrivers,
-		allTeams: (state) => state.allTeams,
-		upcomingRace: (state) => state.upcomingRace,
-		currentRound: (state) => state.currentRound,
-		nextSession: (state) => state.nextSession,
-	},
-	mutations: {
-		SET_RACES: (state, confirmedRaces) => {
-			state.allConfirmedRaces.push(...confirmedRaces)
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage,
+    }),
+  ],
+  state: {
+    allDataLoaded: false,
+    raceDataLoaded: false,
+    raceNav: 'Last Race',
+    standingsNav: 'Driver Standings',
+    allConfirmedRaces: [],
+    allDrivers: [],
+    allTeams: [],
+    upcomingRace: {},
+    currentRound: 0,
+    nextSession: {},
+  },
+  getters: {
+    allConfirmedRaces: (state) => state.allConfirmedRaces,
+    allDrivers: (state) => state.allDrivers,
+    allTeams: (state) => state.allTeams,
+    upcomingRace: (state) => state.upcomingRace,
+    currentRound: (state) => state.currentRound,
+    nextSession: (state) => state.nextSession,
+  },
+  mutations: {
+    SET_RACES: (state, confirmedRaces) => {
+      state.allConfirmedRaces.push(...confirmedRaces)
 
-			state.raceDataLoaded = true
-		},
-		SET_DRIVERS: (state, drivers) => {
-			state.allDrivers.push(...drivers)
-		},
-		SET_TEAMS: (state, teams) => {
-			state.allTeams.push(...teams)
-		},
-		SET_UPCOMING_RACE: (state, upcomingRaceObj) => {
-			state.upcomingRace = upcomingRaceObj
-		},
-		SET_CURRENT_ROUND: (state, round) => {
-			state.currentRound = round
-		},
-		SET_SESSION: (state, currentSession) => {
-			state.nextSession = currentSession
-			state.allDataLoaded = true
-		},
-		SET_NAV_NAME: (state, raceNavValue) => {
-			// Varible used for routing of race.
-			state.raceNav = raceNavValue
-		},
-		SET_STANDINGS_NAME: (state, standingsNavValue) => {
-			// Varible used for routing of standings.
-			state.standingsNav = standingsNavValue
-		},
-	},
-	actions: {
-		async getAllRaces({ commit }) {
-			/*
-        Return all races from season where status != Cancelled
-        Exemple:
-        Input - (2021)
-        Return - [], with all races from the season
+      state.raceDataLoaded = true
+    },
+    SET_DRIVERS: (state, drivers) => {
+      state.allDrivers.push(...drivers)
+    },
+    SET_TEAMS: (state, teams) => {
+      state.allTeams.push(...teams)
+    },
+    SET_UPCOMING_RACE: (state, upcomingRaceObj) => {
+      state.upcomingRace = upcomingRaceObj
+    },
+    SET_CURRENT_ROUND: (state, round) => {
+      state.currentRound = round
+    },
+    SET_SESSION: (state, currentSession) => {
+      state.nextSession = currentSession
+      state.allDataLoaded = true
+    },
+    SET_NAV_NAME: (state, raceNavValue) => {
+      // Varible used for routing of race.
+      state.raceNav = raceNavValue
+    },
+    SET_STANDINGS_NAME: (state, standingsNavValue) => {
+      // Varible used for routing of standings.
+      state.standingsNav = standingsNavValue
+    },
+  },
+  actions: {
+    async getAllRaces({ commit }) {
+      /*
+        Return all races
       */
 
-			const todaysDate = new Date()
-			const season = todaysDate.getFullYear() - 1
-			const confirmedRaces = []
+      const confirmedRaces = []
 
-			try {
-				const response = await axios.get(`${baseURLF1}/races/${season}`, {
-					headers: {
-						"x-rapidapi-key": apiKeyF1,
-						"x-rapidapi-host": hostF1,
-					},
-				})
+      try {
+        const response = await axios.get(`${baseUrl}/races`)
+        console.log(' Store - Fetching races from database')
 
-				response.data.results.forEach((race) => {
-					if (race.status !== "Cancelled") {
-						race.date_in_milli = Date.parse(race.end_date)
-						confirmedRaces.push(race)
-					}
-				})
+        let races = response.data.data[0]['allRaces']
 
-				confirmedRaces.forEach((race, index) => {
-					race.track_id = addTrackId[race.track]
-					race.grand_prix_id = addGrandPrixId[race.name]
-					race.race_round = index + 1
-					if (race.country in changeCountryName) {
-						race.country = changeCountryName[race.country]
-					}
+        races.forEach((race) => {
+          if (race.status !== 'Cancelled') {
+            race.date_in_milli = Date.parse(race.end_date)
+            confirmedRaces.push(race)
+          }
+        })
 
-					race.sessions.forEach((session) => {
-						if (session.session_name === "Sprint Qualifying") {
-							race.sprint_race = true
-						}
-					})
-				})
+        confirmedRaces.forEach((race) => {
+          race.track_id = addTrackId[race.track]
+          race.grand_prix_id = addGrandPrixId[race.name]
+          if (race.country in changeCountryName) {
+            race.country = changeCountryName[race.country]
+          }
 
-				console.log("Fetching allRaces from database")
-				commit("SET_RACES", confirmedRaces)
-			} catch (e) {
-				console.error(e)
-			}
-		},
-		async getAllDrivers({ commit }) {
-			/*
+          race.sessions.forEach((session) => {
+            if (session.session_name === 'Sprint Qualifying') {
+              race.sprint_race = true
+            }
+          })
+        })
+
+        const sortedArray = races.sort(function(a, b) {
+          return new Date(a.end_date) - new Date(b.end_date)
+        })
+
+        console.log('sortedArray', sortedArray)
+
+        commit('SET_RACES', sortedArray)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async getAllDrivers({ commit }) {
+      /*
         Return - [], with all drivers and reserve drivers
       */
 
-			let drivers = []
+      let drivers = []
 
-			try {
-				const response = await axios.get(`${baseUrl}/drivers`)
+      try {
+        const response = await axios.get(`${baseUrl}/drivers`)
 
-				drivers = response.data.data
+        drivers = response.data.data
 
-				console.log("Fetching DRIVERS from database")
-				commit("SET_DRIVERS", drivers)
-			} catch (e) {
-				console.error(e)
-			}
-		},
-		async getAllTeams({ commit }) {
-			/*
+        console.log('Fetching DRIVERS from database')
+        commit('SET_DRIVERS', drivers)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async getAllTeams({ commit }) {
+      /*
         Return - [], with all current teams
       */
 
-			let teams = []
+      let teams = []
 
-			try {
-				const response = await axios.get(`${baseUrl}/teams`)
+      try {
+        const response = await axios.get(`${baseUrl}/teams`)
 
-				teams = response.data.data
+        teams = response.data.data
 
-				console.log("Fetching TEAMS from database")
-				commit("SET_TEAMS", teams)
-			} catch (e) {
-				console.error(e)
-			}
-		},
-		getUpcomingRace({ commit }, allRaces) {
-			/*
+        console.log('Fetching TEAMS from database')
+        commit('SET_TEAMS', teams)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    getUpcomingRace({ commit }, allRaces) {
+      /*
         Return - the next race obj
       */
-			// const nextRaceObj = {}
-			// const nextRaceObj = allRaces.find((race) => race.date_in_milli >= todaysDate)
-			const upcomingRaceObj = allRaces.find(
-				(race) => race.date_in_milli === 1616889600000
-			)
-			// console.log("jbjjjbjbni")
 
-			// allRaces.forEach((race) => {
-			// 	//
-			// 	if (race.date_in_milli === 1616889600000)
-			// })
+      const upcomingRaceObj = allRaces.find(
+        (race) => race.date_in_milli >= todaysDate
+      )
 
-			console.log("Set UPCOMING_RACE")
+      console.log('SET_UPCOMING_RACE', upcomingRaceObj)
 
-			commit("SET_UPCOMING_RACE", upcomingRaceObj)
-		},
-		getNextRaceRound({ commit }, upcomingRace) {
-			/*
+      commit('SET_UPCOMING_RACE', upcomingRaceObj)
+    },
+    getNextRaceRound({ commit }, upcomingRace) {
+      /*
         Return - the current round number
       */
-			// const round = upcomingRace.race_round
-			const round = 1
+      // const round = upcomingRace.race_round
+      const round = upcomingRace.round
 
-			console.log("Set CURRENT_ROUND")
-			commit("SET_CURRENT_ROUND", round)
-		},
-		getNextSession({ commit }, upcomingRace) {
-			/*
+      console.log('Set CURRENT_ROUND', round)
+      commit('SET_CURRENT_ROUND', round)
+    },
+    getNextSession({ commit }, upcomingRace) {
+      /*
         Return - the active session
         If: the active session don't have live data: sessionType, nextSessionName, nextSessionDate
         Else: sessionType and sessionId
       */
-			const relevantSessions = []
-			// let nextSession = {}
+      const relevantSessions = []
+      // let nextSession = {}
 
-			upcomingRace.sessions.forEach((session) => {
-				if (!notWantedSessions.includes(session.session_name)) {
-					session.session_type = newSessionType[session.session_name]
-					relevantSessions.push(session)
-				}
-			})
+      upcomingRace.sessions.forEach((session) => {
+        if (!notWantedSessions.includes(session.session_name)) {
+          session.session_type = newSessionType[session.session_name]
+          relevantSessions.push(session)
+        }
+      })
 
-			// relevantSessions.find((relSession) => {
-			//   console.log('relSession', )
-			//   const dateInMilli = Date.parse(relSession.date)
+      // relevantSessions.find((relSession) => {
+      //   console.log('relSession', )
+      //   const dateInMilli = Date.parse(relSession.date)
 
-			//   if (dateInMilli === todaysDate) {
-			//     nextSession = {
-			//       sessionType: relSession.session_type,
-			//       sessionId: relSession.id,
-			//     }
-			//   }
+      //   if (dateInMilli === todaysDate) {
+      //     nextSession = {
+      //       sessionType: relSession.session_type,
+      //       sessionId: relSession.id,
+      //     }
+      //   }
 
-			//   if (dateInMilli >= todaysDate) {
-			//     nextSession = {
-			//       sessionType: "Next Race",
-			//       sessionName: relSession.session_name,
-			//       startTime: relSession.date,
-			//     }
-			//   }
-			//   return nextSession
-			// })
+      //   if (dateInMilli >= todaysDate) {
+      //     nextSession = {
+      //       sessionType: "Next Race",
+      //       sessionName: relSession.session_name,
+      //       startTime: relSession.date,
+      //     }
+      //   }
+      //   return nextSession
+      // })
 
-			// this.currentSession = nextSession
-			this.currentSession = "Next Race"
-			console.log("Set CURRENT_SESSION")
-			commit("SET_SESSION", this.currentSession)
-		},
-	},
+      // this.currentSession = nextSession
+      this.currentSession = 'Next Race'
+      console.log('Set CURRENT_SESSION', this.currentSession)
+      commit('SET_SESSION', this.currentSession)
+    },
+  },
 })
